36cbd6d17dee49b3d082d2cdd686e8f18ab51d69
leandro-driguez <leandro_driguez@outlook.com>
2024-06-30 21:13:05 -0400

    feat(wc): implement the count of chars

--- wc/ccwc.c
+++ wc/ccwc.c
@@ -1,106 +1,98 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
 #include <string.h>
 #include <wctype.h>
+#include <locale.h>
+#include <wchar.h>
 
-// #FIX this method doesn't count well the amount of words in images 
-long countWords(const char *filename) {
-    FILE *file = fopen(filename, "r"); // Open the file in read mode
-    if (file == NULL) {
-        perror("Failed to open file");
-        return -1;
-    }
+/**
+ * @brief WC structure containing counted bytes, characters, words and lines
+ */
+typedef struct  {  
+  unsigned long bytes;
+  unsigned long chars;
+  unsigned long words;
+  unsigned long lines;
+} WC;
 
-    long amountWords = 0;
+/**
+ * @brief function that calculates chars, words and lines 
+ * 
+ * @param fd file descriptor we are reading from
+ * @return WC struct
+ */
+WC read_from_fd(FILE *fd) {
+    WC counter = { .bytes=0, .chars=0, .lines=0, .words=0};
+
     int thereIsWord = 0;
+    char buff[8];
     wint_t ch; // Variable to store each character read from the file
 
-    // count all words between white spaces
-    while((ch = fgetc(file)) != EOF) {
-        // check if wide character is whitespace - ' ', \n, \t, \r, etc.
+    while ((ch = fgetc(fd)) != WEOF) {
+        // count all words between whitespaces
         if (iswspace(ch) && thereIsWord) {
-            amountWords++;
+            counter.words++;
             thereIsWord = 0;
         }
         else if (!iswspace(ch))  {   
             thereIsWord = 1;
-        }
+        } // #FIX this method doesn't count well the amount of words in images 
+
+        // add new line
+        if (ch == L'\n') counter.lines++;
+
+        // add character
+        counter.chars++;
+
+        // convert wide character to multibyte and add the lenght to the sum of bytes
+        counter.bytes += wctomb(buff, ch);
     }
 
-    // at the end of the file there is a word 
-    if (thereIsWord) amountWords++;
-
-    return amountWords;
-}
-
-int countLines(const char *filename) {
-    FILE *file = fopen(filename, "r"); // Open the file in read mode
-    if (file == NULL) {
-        perror("Failed to open file");
-        return -1;
-    }
-
-    int amountLines = 0;
-    wint_t ch; // Variable to store each character read from the file
-
-    while ((ch = fgetc(file)) != EOF) {
-        if (ch == '\n') {
-            amountLines++; // Increment the count for each newline character
-        }
-    }
-
-    fclose(file); // Close the file
-    return amountLines;
-}
-
-long getFileSize(const char *filename) {
-    FILE *file = fopen(filename, "rb"); // Open the file in binary mode
-    if (file == NULL) {
-        perror("Failed to open file");
-        return -1;
-    }
-
-    // Seek to the end of the file
-    if (fseek(file, 0, SEEK_END) != 0) {
-        perror("Failed to seek to end of file");
-        fclose(file);
-        return -1;
-    }
-
-    // Get the current file pointer position, which is the file size in bytes
-    long fileSize = ftell(file);
-    if (fileSize == -1L) {
-        perror("Failed to get file size");
-    }
-
-    fclose(file); // Close the file
-    return fileSize;
+    return counter;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s [OPTION] <filename>\n", argv[0]);
-        return 1;
+    // Locale must be set in order to properly work with wide characters
+    setlocale(LC_ALL, "");
+
+    char *filename = NULL;
+    if (argc == 3) filename = argv[2];
+
+    FILE *fd;
+
+    if (filename) {
+        fd = fopen(argv[2], "r"); // Open the file
+        if (fd == NULL) {
+            perror("Failed to open file");
+            return -1;
+        }
     }
+    else if (filename) {
+        fd = stdin;
+    }
+
+    WC counter = read_from_fd(fd);
+
+    fclose(fd);
 
     long result;
-
     if (!strcmp(argv[1], "-c")) {
-        result = getFileSize(argv[2]);
+        printf("%ld", counter.bytes);
     }
     else if (!strcmp(argv[1], "-l")) {
-        result = (long)countLines(argv[2]);
+        printf("%ld", counter.lines);
     }
     else if (!strcmp(argv[1], "-w")) {
-        result = countWords(argv[2]);
+        printf("%ld", counter.words);
     }
-    else {
-        perror("The [OPTION] passed was wrong");
-        result = -1;
+    else if (!strcmp(argv[1], "-m")) {
+        printf("%ld", counter.chars);
     }
+    if(filename) printf(" %s", filename);
 
-    if (result != -1) {
-        printf("%ld %s\n", result, argv[2]);
-    }
+    printf("\n");
 
     return 0;
 }
